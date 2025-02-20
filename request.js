ad4f54f09e0cf2322762493f1a25536f19f270f5
akashpatel121 <akashpatel522004@gmail.com>
2025-02-20 14:36:33 +0530

    Request and Image analysis

---
+++ request.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+let request = "I want my Aadhar card";
+const prompt = `What is one word for  "${request}"`;
+
+async function generateContent() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+}
+
+generateContent().catch(console.error);
